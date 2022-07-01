67475822d41a57a914d4776cc3560e0b974693a1
happychuwy <elihuromero@outlook.com>
2022-06-30 22:37:12 -0500

    probando repo

---
+++ index.php
@@ -0,0 +1,18 @@
+<html>
+    <head>
+        <title>
+            maquetado web
+        </title>
+        
+    </head>
+    <body>
+
+    <div>
+        maqueta el siguiente sitio: https://mammoth.tech/
+    </div>
+        
+    </body>
+    <script>
+        console.log("prueba1")
+    </script>
+</html>
